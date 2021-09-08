654019d7a4eb0d561eda5b779bce308f3cebd2d7
Danil Butenko <danilb@sytoss.intra>
2021-09-08 13:06:13 +0300

    Added communication

--- src/main/java/com/sytoss/model/communication/Communication.java
+++ src/main/java/com/sytoss/model/communication/Communication.java
@@ -1,10 +1,42 @@
 package com.sytoss.model.communication;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
+import java.util.Date;
 
 @Entity
-@Table()
+@Table(name = "communication")
 public class Communication {
+
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "sender_id", nullable = false)
+    private Long senderId;
+
+    @Column(name = "send_date", nullable = false)
+    private Date sendDate;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "communication_type", nullable = false)
+    private Long communicationTypeId;
+
+    @Column(name = "receiver_id")
+    private Long receiverId;
+
+    @Column(name = "lesson_id")
+    private Long lessonId;
+
+    @Column(name = "homework_id")
+    private Long homeworkId;
+
+    @Column(name = "update_date")
+    private Date updateDate;
+
 
 }
