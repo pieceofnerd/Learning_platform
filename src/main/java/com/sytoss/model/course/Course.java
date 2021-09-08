3b8357263c7df0c640788b345402ab37e22f2f60
peaceofnerd <dari.dorenska@gmail.com>
2021-09-08 12:54:08 +0300

    make a course model

--- src/main/java/com/sytoss/model/course/Course.java
+++ src/main/java/com/sytoss/model/course/Course.java
@@ -1,6 +1,9 @@
 package com.sytoss.model.course;
 
+import com.sytoss.model.Lookup;
+
 import javax.persistence.*;
+
 
 @Entity
 @Table
@@ -10,6 +13,31 @@ public class Course {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
+    @Column(unique = true)
+    private String name;
+
+    @Column
+    private String description;
+
+    @JoinColumn(name = "course_photo_id")
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Lookup coursePhoto;
+
+    @Column(name = "recommended_literature")
+    private String recommendedLiterature;
+
+    @JoinColumn(name = "certificate_template_id")
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Lookup certificateTemplate;
+
+    @Column
+    private Double rating;
+
+
+    @JoinColumn(name = "category_id")
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private Category category;
+
+    public Course() {
+    }
 }
