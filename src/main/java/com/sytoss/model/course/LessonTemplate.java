9ca15938fb42db2ff334052bacd0215992c3dab1
peaceofnerd <dari.dorenska@gmail.com>
2021-09-08 13:13:36 +0300

    edit course

--- src/main/java/com/sytoss/model/course/LessonTemplate.java
+++ src/main/java/com/sytoss/model/course/LessonTemplate.java
@@ -1,4 +1,36 @@
 package com.sytoss.model.course;
 
+import com.sytoss.model.Lookup;
+import jakarta.validation.constraints.NotBlank;
+
+import javax.persistence.*;
+
+@Entity
+@Table
 public class LessonTemplate {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotBlank
+    @Column
+    private String name;
+
+    @Column
+    private String description;
+
+    @JoinColumn(name = "media_type")
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Lookup media;
+
+    @Column
+    private Integer duration;
+
+    @JoinColumn(name = "topic_id")
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    private Topic topic;
+
+    public LessonTemplate() {
+    }
 }
--- src/main/java/com/sytoss/model/course/Topic.java
+++ src/main/java/com/sytoss/model/course/Topic.java
@@ -1,6 +1,9 @@
 package com.sytoss.model.course;
 
+import jakarta.validation.constraints.NotBlank;
+
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table
@@ -10,12 +13,20 @@ public class Topic {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotBlank
     @Column
     private String name;
 
+    @Column
     private String description;
 
     @JoinColumn(name = "course_id")
     @ManyToOne(optional = false, fetch = FetchType.LAZY)
     private Course course;
+
+    @OneToMany(mappedBy = "topic")
+    private List<LessonTemplate> lessonTemplates;
+
+    public Topic() {
+    }
 }
