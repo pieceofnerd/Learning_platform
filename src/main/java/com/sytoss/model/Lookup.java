1caa8b684e450d7186c61596499042265d80527c
peaceofnerd <dari.dorenska@gmail.com>
2021-09-08 13:44:17 +0300

    edit tables

--- src/main/java/com/sytoss/model/Lookup.java
+++ src/main/java/com/sytoss/model/Lookup.java
@@ -3,7 +3,7 @@ package com.sytoss.model;
 import javax.persistence.*;
 
 @Entity
-@Table
+@Table(name = "lookup")
 public class Lookup {
 
     @Id
--- src/main/java/com/sytoss/model/course/Category.java
+++ src/main/java/com/sytoss/model/course/Category.java
@@ -6,7 +6,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table
+@Table(name = "category")
 public class Category {
 
     @Id
