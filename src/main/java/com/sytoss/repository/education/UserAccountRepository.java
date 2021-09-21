7e9602fc57cabd78a1bc7d7cf2c65c1742e0ffbc
danilb <butenkodanil84@gmail.com>
2021-09-21 16:36:56 +0300

    Added new methods

--- src/main/java/com/sytoss/repository/education/UserAccountRepository.java
+++ src/main/java/com/sytoss/repository/education/UserAccountRepository.java
@@ -8,11 +8,18 @@ import java.util.List;
 
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 
-    @Query("select u from UserAccount u where u.email = ?1")
-    UserAccount findUserAccountByEmail(String email);
+
+    @Query("select u from UserAccount u where u.email = ?1 and u.deleted = false")
+    UserAccount findUserAccountByEmailAndDeletedIsFalse(String email);
+
+    @Query("select u from UserAccount u where upper(u.firstName) like upper(concat(?1, '%')) and upper(u.secondName) like upper(concat(?2, '%')) and u.deleted = false")
+    List<UserAccount> findAllByFirstNameStartingWithIgnoreCaseAndSecondNameStartingWithIgnoreCaseAndDeletedIsFalse(String firstName, String secondName);
 
 
+    @Query("select u from UserAccount u where upper(u.firstName) like upper(concat(?1, '%')) and u.deleted = false")
+    List<UserAccount> findAllByFirstNameStartingWithIgnoreCaseAndDeletedIsFalse(String firstName);
 
-    @Query("select u from UserAccount u where upper(u.firstName) like upper(concat(?1, '%')) and upper(u.secondName) like upper(concat(?2, '%'))")
-    List<UserAccount> findAllByFirstNameStartingWithIgnoreCaseAndSecondNameStartingWithIgnoreCase(String firstName, String secondName);
+
+    @Query("select u from UserAccount u where upper(u.secondName) like upper(concat(?1, '%')) and u.deleted = false")
+    List<UserAccount> findAllBySecondNameStartingWithIgnoreCaseAndDeletedIsFalse(String secondName);
 }
