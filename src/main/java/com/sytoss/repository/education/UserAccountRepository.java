e2a2483180463c2951d57b4b891df40be920fb7e
danilb <butenkodanil84@gmail.com>
2021-09-21 13:37:18 +0300

    Changed return type in CommunicationRepository method, added new method in UserAccountRepository

--- src/main/java/com/sytoss/repository/education/UserAccountRepository.java
+++ src/main/java/com/sytoss/repository/education/UserAccountRepository.java
@@ -4,9 +4,15 @@ import com.sytoss.model.education.UserAccount;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+
 public interface UserAccountRepository extends JpaRepository<UserAccount, Long> {
 
     @Query("select u from UserAccount u where u.email = ?1")
     UserAccount findUserAccountByEmail(String email);
 
+
+
+    @Query("select u from UserAccount u where upper(u.firstName) like upper(concat(?1, '%')) and upper(u.secondName) like upper(concat(?2, '%'))")
+    List<UserAccount> findAllByFirstNameStartingWithIgnoreCaseAndSecondNameStartingWithIgnoreCase(String firstName, String secondName);
 }
