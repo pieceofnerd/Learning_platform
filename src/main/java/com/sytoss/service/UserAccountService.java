b92ab3f2038c94591d072dede0002b840f46d6b5
danilb <butenkodanil84@gmail.com>
2021-09-21 13:50:34 +0300

    Remove findUserAccountById from UserAccountService

--- src/main/java/com/sytoss/service/UserAccountService.java
+++ src/main/java/com/sytoss/service/UserAccountService.java
@@ -2,16 +2,13 @@ package com.sytoss.service;
 
 import com.sytoss.model.communication.Communication;
 import com.sytoss.model.education.UserAccount;
-import com.sytoss.web.dto.FilterDTO;
-import com.sytoss.web.dto.UserAccountDTO;
 import com.sytoss.web.dto.filter.FilterUserAccountDTO;
-import com.sytoss.web.dto.save.UserAccountSaveDTO;
 
 import java.util.List;
 
 public interface UserAccountService {
 
-    UserAccount findUserAccountById(Long id) throws Exception;
+//    UserAccount findUserAccountById(Long id) throws Exception;
 
     List<UserAccount> findUsersByFilter(FilterUserAccountDTO filter);
 
