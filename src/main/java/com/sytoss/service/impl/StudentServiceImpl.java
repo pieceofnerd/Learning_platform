0327029be64f784aabc5e13101d8cf5570ddc824
6uten <butenkodanil84@gmail.com>
2021-09-24 15:54:43 +0300

    Added new method implementations payment and student joining the study group.

--- src/main/java/com/sytoss/service/impl/StudentServiceImpl.java
+++ src/main/java/com/sytoss/service/impl/StudentServiceImpl.java
@@ -3,8 +3,14 @@ package com.sytoss.service.impl;
 import com.sytoss.model.course.Course;
 import com.sytoss.model.course.StudyGroup;
 import com.sytoss.model.education.Purchase;
+import com.sytoss.model.education.Study;
 import com.sytoss.model.education.UserAccount;
+import com.sytoss.repository.education.StudyRepository;
 import com.sytoss.service.StudentService;
+import com.sytoss.service.StudyGroupService;
+import com.sytoss.service.StudyService;
+import com.sytoss.web.dto.filter.Filter;
+import com.sytoss.web.dto.filter.FilterStudyDTO;
 import com.sytoss.web.dto.filter.FilterUserAccountDTO;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -17,6 +23,9 @@ import java.util.List;
 @RequiredArgsConstructor
 public class StudentServiceImpl implements StudentService {
 
+    private final StudyRepository studyRepository;
+    private final StudyService studyService;
+    private final StudyGroupService studyGroupService;
 
     @Override
     public void rateCourse(UserAccount student, Course course, Integer rateValue) {
@@ -44,12 +53,17 @@ public class StudentServiceImpl implements StudentService {
     }
 
     @Override
-    public void joinStudyGroup(UserAccount student, Course course) {
-        //TODO
+    public void joinStudyGroup(UserAccount student, StudyGroup studyGroup) throws Exception {
+        studyService.saveStudy(student,studyGroup);
     }
 
     @Override
-    public void leaveStudyGroup(UserAccount student, StudyGroup studyGroup) {
-        //TODO
+    public void leaveStudyGroup(UserAccount student, StudyGroup studyGroup) throws Exception {
+
+        //when a student leaves the group, all studies of the student in this group are deleted.
+        Study study = studyRepository.findStudyByStudentAndStudyGroup(student, studyGroup);
+        if (study != null) {
+            studyService.deleteStudy(study);
+        }
     }
 }
